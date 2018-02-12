97284ce857b08783b64de2ff337c116b9086578d
emekoi <emekankurumeh@outlook.com>
2018-02-12 00:56:39 -0600

    adding copyPixels function

--- src/suffer.nim
+++ src/suffer.nim
@@ -92,26 +92,26 @@ proc cloneBuffer*(src: Buffer): Buffer
   ## creates a copy of the buffer
 proc loadPixels*(buf: Buffer, src: openarray[uint8], fmt: PixelFormat)
   ## loads the data from `src` into the buffer using the given pixel format
-proc loadPixels8*(buf: var Buffer, src: openarray[uint8], pal: openarray[Pixel])
+proc loadPixels8*(buf: Buffer, src: openarray[uint8], pal: openarray[Pixel])
   ## loads the data from `src` into the buffer using the given palette
-proc loadPixels8*(buf: var Buffer, src: openarray[uint8])
+proc loadPixels8*(buf: Buffer, src: openarray[uint8])
   ## loads the data from `src` into the buffer, using it set the alpha of all its pixels
-proc setBlend*(buf: var Buffer, blend: BlendMode)
+proc setBlend*(buf: Buffer, blend: BlendMode)
   ## sets the buffer's blend mode
-proc setAlpha*[T](buf: var Buffer, alpha: T)
+proc setAlpha*[T](buf: Buffer, alpha: T)
   ## sets the buffer's alpha
-proc setColor*(buf: var Buffer, c: Pixel)
+proc setColor*(buf: Buffer, c: Pixel)
   ## sets the buffer's color
-proc setClip*(buf: var Buffer, r: Rect)
+proc setClip*(buf: Buffer, r: Rect)
   ## sets the buffer's clipping rectanlge
-proc reset*(buf: var Buffer)
+proc reset*(buf: Buffer)
   ## resets the buffer to a default state
 proc clear*(buf: Buffer, c: Pixel)
   ## sets the buffer's blend mode
 proc getPixel*(buf: Buffer, x: int, y: int): Pixel
-  ## gets the pixels at (x, y) on the buffer
+  ## gets the color of the pixel at (x, y) on the buffer
 proc setPixel*(buf: Buffer, c: Pixel, x: int, y: int)
-  ## sets the pixels at (x, y) on the buffer
+  ## sets the color of the pixel at (x, y) on the buffer
 
 # proc copyPixels*(buf: Buffer, src: Buffer, x: int, y: int, sub:  Rect, sx: float, sy: float)
 # proc noise*(buf: Buffer, seed: cuint, low: int, high: int, grey: int)
@@ -145,7 +145,7 @@ type
   Point = tuple
     x, y: int
 
-  RandState = tuple
+  RandState = ref tuple
     x, y, z, w: uint
 
 proc xdiv[T](n, x: T): T =
@@ -153,12 +153,13 @@ proc xdiv[T](n, x: T): T =
   return n div x
 
 proc rand128init(seed: uint): RandState =
+  result = new RandState
   result.x = (seed and 0xff000000'u) or 1'u
   result.y = seed and 0xff0000'u
   result.z = seed and 0xff00'u
   result.w = seed and 0xff'u
 
-proc rand128(s: var RandState): uint =
+proc rand128(s: RandState): uint =
   result = s.x xor (s.x shl 11'u)
   s.x = s.y; s.y = s.z; s.z = s.w
   s.w = s.w xor (s.w shr 19) xor result xor (result shr 8)
@@ -200,17 +201,21 @@ proc clipRectAndOffset(r: var Rect, x, y: var int, to: Rect) =
   if (y + r.h) - (to.y + to.h) > 0:
     r.h -= (y + r.h) - (to.y + to.h)
 
+proc initBuffer(buf: Buffer, w, h: int) =
+  buf.w = w; buf.h = h
+  buf.reset()
+
 proc newBuffer*(w, h: int): Buffer =
   result = new BufferOwned
   result.pixels = repeat(color(0, 0, 0), w * h)
-  result.w = w; result.h = h
-  result.reset()
+  # initialize the buffer
+  initBuffer(result, w, h)
 
 proc newBufferShared*(pixels: pointer, w, h: int): Buffer =
   result = new BufferShared
   result.pixels = pixels
-  result.w = w; result.h = h
-  result.reset()
+  # initialize the buffer
+  initBuffer(result, w, h)
 
 proc cloneBuffer*(src: Buffer): Buffer =
   deepCopy(result, src)
@@ -230,31 +235,31 @@ proc loadPixels*(buf: Buffer, src: openarray[uint8], fmt: PixelFormat) =
     buf.pixels[i].rgba.b = (src[i] shr sb) and 0xff
     buf.pixels[i].rgba.a = (src[i] shr sa) and 0xff
 
-proc loadPixels8*(buf: var Buffer, src: openarray[uint8], pal: openarray[Pixel]) =
+proc loadPixels8*(buf: Buffer, src: openarray[uint8], pal: openarray[Pixel]) =
   let sz = (buf.w * buf.h) - 1
   for i in countdown(sz, 0):
     buf.pixels[i] = pal[src[i]]
 
-proc loadPixels8*(buf: var Buffer, src: openarray[uint8]) =
+proc loadPixels8*(buf: Buffer, src: openarray[uint8]) =
   let sz = (buf.w * buf.h) - 1
   for i in countdown(sz, 0):
     buf.pixels[i] = pixel(0xff'u8, 0xff'u8, 0xff'u8, src[i])
 
-proc setBlend*(buf: var Buffer, blend: BlendMode) =
+proc setBlend*(buf: Buffer, blend: BlendMode) =
   buf.mode.blend = blend
 
-proc setAlpha*[T](buf: var Buffer, alpha: T) =
+proc setAlpha*[T](buf: Buffer, alpha: T) =
   buf.mode.alpha = clamp(alpha, 0, 0xff)  
 
-proc setColor*(buf: var Buffer, c: Pixel) =
+proc setColor*(buf: Buffer, c: Pixel) =
   buf.mode.color.word = c.word & RGB_MASK 
 
-proc setClip*(buf: var Buffer, r: Rect) =
+proc setClip*(buf: Buffer, r: Rect) =
   buf.clip = r
   var r = Rect(0, 0, buf.w, buf.h)
   clipRect(buf.clip, r)
 
-proc reset*(buf: var Buffer) =
+proc reset*(buf: Buffer) =
   buf.setBlend(ALPHA)
   buf.setAlpha(0xff)
   buf.setColor color(0xff, 0xff, 0xff)
@@ -272,6 +277,31 @@ proc getPixel*(buf: Buffer, x: int, y: int): Pixel =
 proc setPixel*(buf: Buffer, c: Pixel, x: int, y: int) =
   if (x >= 0 and y >= 0 and x < buf.w and y < buf.h):
     buf.pixels[x + y * buf.w] = c
+
+proc copyPixelsBasic(buf, src: Buffer, x, y: int, sub: Rect) =
+  # Clip to destination buffer
+  clipRectAndOffset(var sub, var x, var y, buf.clip)
+  # Clipped off screen?
+  if sub.w <= 0 or sub.h <= 0: return
+  # Copy pixels
+  for i in 0..<sub.h:
+    case buf
+    of BufferOwned:
+      case src:
+      of BufferOwned:
+        buf.pixels[(x + (y + i) * buf.w)..sub.w]
+      of BufferShared:
+        discard
+    of BufferShared:
+      case src:
+      of BufferOwned:
+        discard
+      of BufferShared:
+        discard
+    # copyMem addr buf.pixels, addr src.pix
+    # memcpy(b->pixels + x + (y + i) * b->w,
+    #        src->pixels + s.x + (s.y + i) * src->w,
+    #        s.w * sizeof(*b->pixels));
 
 # proc copyPixels*(buf: Buffer, src: Buffer, x: int, y: int, sub:  Rect, sx: float, sy: float)
 # proc noise*(buf: Buffer, seed: cuint, low: int, high: int, grey: int)
